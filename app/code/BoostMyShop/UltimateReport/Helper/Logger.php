cf298aaa2f7bff9cb53990fcd52cfab2473a4949
Dmitry Fedyuk <dfediuk@gmail.com>
2020-09-26 15:31:01 +0600

    https://github.com/dxmoto/site/issues/106

--- app/code/BoostMyShop/UltimateReport/Helper/Logger.php
+++ app/code/BoostMyShop/UltimateReport/Helper/Logger.php
@@ -10,6 +10,9 @@ class Logger
 
     public function log($msg, $type = self::kLogGeneral)
     {
+    	# 2020-09-26 Dmitry Fedyuk https://www.upwork.com/fl/mage2pro
+		# "Disable `ultimatereport_general.log`": https://github.com/dxmoto/site/issues/106
+    	return;
         $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/ultimatereport_'.$type.'.log');
         $logger = new \Zend\Log\Logger();
         $logger->addWriter($writer);
